c7a5c80668b1a87a762e2634f7dda5fea38b2eff
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-07-22 21:49:59 +0200

    go functions

--- test02.go
+++ test02.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	for i := 0; i < 5; i++ {
-		fmt.Println("value = ", i)
+func sayHello(n string) {
+	fmt.Printf("Hello %v \n", n)
+}
+
+func cycleNames(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
 	}
+}
+
+func main() {
+	cycleNames([]string{"mario", "peach", "bowser"}, sayHello)
 }
